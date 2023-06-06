9a104229feda36e4ac601cb86838ae018da4b4fb
dkaramardian <dk11@stanford.edu>
2023-06-05 22:06:38 -0700

    updates to eval

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,11 @@
 import argparse
 from collections import Counter
+import csv
+import matplotlib.pyplot as plt
+import os
 import pandas as pd
 
-def calculate_reward(data, ngames):
+def qlearning_reward(data, ngames):
     value = 0
     for i in range(ngames):
         df = data[(i*5):((i*5)+5)]
@@ -13,17 +16,103 @@ def calculate_reward(data, ngames):
     value = value/ngames
     return value
 
-def calculate_profit(data, ngames):
+def qlearning_profit(data, ngames):
+    value = 0
+    for i in range(ngames):
+        df = data[(i*5):((i*5)+5)]
+        q_val = df.at[i*5, 'q_score']
+        for index, row in df.iterrows():
+            if row['exercise']:
+                reward = row['reward']
+                break
+        value += reward - q_val
+    value = value/ngames
+    return value
+
+def max_reward(data, ngames):
+    value = 0
+    for i in range(ngames):
+        df = data[(i*5):((i*5)+5)]
+        max = 0
+        for index, row in df.iterrows():
+            if row['reward'] > max:
+                max = row['reward']
+        value += max
+    value = value/ngames
+    return value
+
+def max_profit(data, ngames):
+    value = 0
+    for i in range(ngames):
+        df = data[(i*5):((i*5)+5)]
+        max = 0
+        q_val = df.at[i*5, 'q_score']
+        for index, row in df.iterrows():
+            if row['reward'] > max:
+                max = row['reward']
+        value += max - q_val
+    value = value/ngames
+    return value
+
+def pos_reward(data, ngames):
     value = 0
     for i in range(ngames):
         df = data[(i*5):((i*5)+5)]
         reward = 0
-        q_val = 0
         for index, row in df.iterrows():
-            q_val = df.at[i*5, 'q_score']
-            if row['exercise']:
+            if row['reward'] > reward:
                 reward = row['reward']
                 break
+        value += reward
+    value = value/ngames
+    return value
+
+def pos_profit(data, ngames):
+    value = 0
+    for i in range(ngames):
+        df = data[(i*5):((i*5)+5)]
+        reward = 0
+        q_val = df.at[i*5, 'q_score']
+        for index, row in df.iterrows():
+            if row['reward'] > reward:
+                reward = row['reward']
+                break
+        value += reward - q_val
+    value = value/ngames
+    return value
+
+def q2_reward(data, ngames):
+    value = 0
+    for i in range(ngames):
+        df = data[(i*5):((i*5)+5)]
+        value += df.at[(i*5)+2, 'reward']
+    value = value/ngames
+    return value
+
+def q2_profit(data, ngames):
+    value = 0
+    for i in range(ngames):
+        df = data[(i*5):((i*5)+5)]
+        reward = df.at[(i*5)+2, 'reward']
+        q_val = df.at[i*5, 'q_score']
+        value += reward - q_val
+    value = value/ngames
+    return value
+
+def q4_reward(data, ngames):
+    value = 0
+    for i in range(ngames):
+        df = data[(i*5):((i*5)+5)]
+        value += df.at[(i*5)+4, 'reward']
+    value = value/ngames
+    return value
+
+def q4_profit(data, ngames):
+    value = 0
+    for i in range(ngames):
+        df = data[(i*5):((i*5)+5)]
+        reward = df.at[(i*5)+4, 'reward']
+        q_val = df.at[i*5, 'q_score']
         value += reward - q_val
     value = value/ngames
     return value
@@ -53,7 +142,7 @@ if __name__ == "__main__":
     # Create an argument parser
     parser = argparse.ArgumentParser(description="Model Evaluation")
     parser.add_argument("--scored_data_path", type=str, nargs='?', 
-                        default="scored_data/scored_nba_games_2019-20.csv", 
+                        default="scored_data/dqn/scored_nba_games_2019-20_corrected.csv", 
                         help="Path to the input CSV file")
     parser.add_argument("--qvalfunction", type=str, nargs='?', 
                         default="DQN", 
@@ -67,18 +156,39 @@ if __name__ == "__main__":
     nrows = len(scored_data)
     ngames = int(nrows/5)
 
-    # Calculate the reward, profit, and exercise distribution
+    # Calculate results
     results = [
-        {"Reward": calculate_reward(scored_data, ngames), 
-        "Profit": calculate_profit(scored_data, ngames), 
-        "Distribution": exercise_distr(scored_data, ngames)}
+        {"Q Value Function": args.qvalfunction,
+        "Policy Reward": qlearning_reward(scored_data, ngames), 
+        "Policy Profit": qlearning_profit(scored_data, ngames), 
+        "Max Reward": max_reward(scored_data, ngames),
+        "Max Profit": max_profit(scored_data, ngames),
+        "Positive Reward": pos_reward(scored_data, ngames),
+        "Positive Profit": pos_profit(scored_data, ngames),
+        "Q2 Reward": q2_reward(scored_data, ngames),
+        "Q2 Profit": q2_profit(scored_data, ngames),
+        "Q4 Reward": q4_reward(scored_data, ngames),
+        "Q4 Profit": q4_profit(scored_data, ngames)}
     ]
-    reward = calculate_reward(scored_data, ngames)
-    profit = calculate_profit(scored_data, ngames)
-    distr = exercise_distr(scored_data, ngames)
 
-    # Normalize the features
-    print(reward)
-    print(profit)
-    print(distr)
+    # Output results to csv
+    export_to_csv(results, "results.csv")
+
+    # Create Distribution Plot
+    distr = exercise_distr(scored_data, ngames)
+    sorted_keys = sorted(distr.keys())
+    sorted_values = [distr[key] for key in sorted_keys]
+    total = sum(sorted_values)
+    percentages = [value / total * 100 for value in sorted_values]
+    bars = plt.bar(sorted_keys, percentages, color='lightblue')
+    for bar, percentage in zip(bars, percentages):
+        plt.text(bar.get_x() + bar.get_width() / 2, bar.get_height(), 
+                f'{percentage:.1f}%', ha='center', va='bottom')
+    plt.xticks(sorted_keys)
+    plt.xlabel('Quarters')
+    plt.ylabel('Percentage')
+    plt.title('Distribution of {} Exercise Times'.format(args.qvalfunction))
+    plt.savefig('charts/{}_distr_barchart.png'.format(args.qvalfunction))
+
+    #print(results)
     #revenue of each strtegy and profit of each strategy, and what percent of time model has you excercise in a certain quarter
